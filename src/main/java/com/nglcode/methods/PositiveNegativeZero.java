ededcb7e5396b6856878e3b64b540412ba2f5a72
nglcode <nglcode@mailbox.org>
2023-02-28 12:19:17 +0100

    adding first two exercises

---
+++ src/main/java/com/nglcode/methods/PositiveNegativeZero.java
@@ -0,0 +1,23 @@
+package com.nglcode.methods;
+
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+
+        checkNumber(7);
+        checkNumber(-1);
+        checkNumber(0);
+
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
---
+++ src/main/java/com/nglcode/methods/SpeedConverter.java
@@ -0,0 +1,29 @@
+package com.nglcode.methods;
+
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        printConversion(10.25);
+        printConversion(75.114);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        double kphConversion = 1.609;
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / kphConversion);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
