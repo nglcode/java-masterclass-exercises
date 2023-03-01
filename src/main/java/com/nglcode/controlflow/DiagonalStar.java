181c3f1bb07a83b64825be0d86e3ef6c42b645af
nglcode <nglcode@mailbox.org>
2023-03-01 12:31:46 +0100

    diagonal star

---
+++ src/main/java/com/nglcode/controlflow/DiagonalStar.java
@@ -0,0 +1,38 @@
+package com.nglcode.controlflow;
+
+public class DiagonalStar {
+
+    public static void main(String[] args) {
+
+        printSquareStar(5);
+        printSquareStar(8);
+
+    }
+
+    public static void printSquareStar(int number) {
+
+        if ( number < 5 ) {
+            System.out.println("Invalid Value");
+        }
+
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= number; col++) {
+                if ( row == 1 || row == number) {
+                    System.out.print("*");
+                } else if ( col == 1 ) {
+                    System.out.print("*");
+                } else if ( col == number ) {
+                    System.out.print("*");
+                } else if ( col == row ) {
+                    System.out.print("*");
+                } else if ( col == number-row+1 ) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+}
