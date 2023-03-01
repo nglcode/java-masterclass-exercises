6254bbf6e6b3aae1d4e20cb805435544439af778
nglcode <nglcode@mailbox.org>
2023-03-01 11:55:22 +0100

    largest prime alt

--- src/main/java/com/nglcode/controlflow/LargestPrime.java
+++ src/main/java/com/nglcode/controlflow/LargestPrime.java
@@ -38,5 +38,13 @@ public class LargestPrime {
 
     }
 
+    public static int getLargestPrimeAlt (int number) {
+        if (number<2) return -1;
+        for (int i=number/2; i>1; i--) {
+            if(number%i==0) number=i;
+        }
+        return number;
+    }
+
 
 }
