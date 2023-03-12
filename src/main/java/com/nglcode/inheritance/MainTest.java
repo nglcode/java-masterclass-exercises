3bd07eabcee27bf1b1569e79c2280caa9bc4dec2
nglcode <nglcode@mailbox.org>
2023-03-12 20:45:18 +0100

    last modifications

--- src/main/java/com/nglcode/inheritance/MainTest.java
+++ src/main/java/com/nglcode/inheritance/MainTest.java
@@ -4,48 +4,8 @@ public class MainTest {
 
     public static void main(String[] args) {
 
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
-        simpleCalculator.setFirstNumber(5.0);
-        simpleCalculator.setSecondNumber(4);
-        System.out.println("add= " + simpleCalculator.getAdditionResult());
-        System.out.println("subtract= " + simpleCalculator.getSubtractionResult());
-        simpleCalculator.setFirstNumber(5.25);
-        simpleCalculator.setSecondNumber(0);
-        System.out.println("multiply= " + simpleCalculator.getMultiplicationResult());
-        System.out.println("divide= " + simpleCalculator.getDivisionResult());
-
-        System.out.println("=====================");
-        Point first = new Point(6, 5);
-        Point second = new Point(3, 1);
-        System.out.println("distance(0,0)= " + first.distance());
-        System.out.println("distance(second)= " + first.distance(second));
-        System.out.println("distance(2,2)= " + first.distance(2, 2));
-        Point point = new Point();
-        System.out.println("distance()= " + point.distance());
-
-        System.out.println("=====================");
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.0);
-        Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
-
-        System.out.println("=====================");
-        ComplexNumber one = new ComplexNumber(1.0, 1.0);
-        ComplexNumber number = new ComplexNumber(2.5, -1.5);
-        one.add(1,1);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        one.subtract(number);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        number.subtract(one);
-        System.out.println("number.real= " + number.getReal());
-        System.out.println("number.imaginary= " + number.getImaginary());
 
 
     }
+
 }
