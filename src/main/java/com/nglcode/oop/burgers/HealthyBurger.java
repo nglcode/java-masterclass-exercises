c38cbdb05475a37046937e5f2e64ee9be36b790e
nglcode <nglcode@mailbox.org>
2023-03-28 23:30:54 +0200

    healthy burger

--- src/main/java/com/nglcode/oop/burgers/HealthyBurger.java
+++ src/main/java/com/nglcode/oop/burgers/HealthyBurger.java
@@ -2,9 +2,27 @@ package com.nglcode.oop.burgers;
 
 public class HealthyBurger extends Hamburger {
 
-    
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
 
-    public HealthyBurger(String name, String meat, double price, String breadRollType) {
-        super(name, meat, price, breadRollType);
+    public HealthyBurger(String meat, double price) {
+        super("Healthy Burger", meat, price, "Brown rye roll");
+    }
+
+    public void addHealthyAddition1(String name, double price) {
+        healthyExtra1Name = name;
+        healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        healthyExtra2Name = name;
+        healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizehamburger() {
+        return 0.0;
     }
 }
